1d7dd61da9dc5753eb6b1f486dc299a699723342
Anshul <anshul524@gmail.com>
2020-09-15 13:17:01 +0530

    angular-onInIt-Interface

---
+++ angular-onInIt-Interface/src/app/posts/posts.component.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+
+  posts: any;
+  private url = "https://jsonplaceholder.typicode.com/posts";
+
+  constructor(private httpClient: HttpClient) {
+    httpClient.get(this.url)
+            .subscribe(response => {
+              this.posts = response;
+            });        
+  }
+
+  createPost(input: HTMLInputElement){
+    let post = { title: input.value };
+    input.value = "";
+    this.httpClient.post(this.url,JSON.stringify(post))
+        .subscribe(response => {
+          post['id'] = response.id;
+          this.posts.splice(0,0,post);
+          console.log(response);
+        });
+  }
+
+  updatePost(post){
+    this.httpClient.patch(this.url + "/" + post.id, JSON.stringify({isRead: true}))
+        .subscribe(response => {
+          console.log(response);
+        });    
+  }
+
+  deletePost(post){
+    this.httpClient.delete(this.url + "/" + post.id)
+        .subscribe(response => {
+          let index = this.posts.indexOf(post);
+          this.posts.splice(index,1);
+        });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
